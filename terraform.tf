89a7ad4b7394b19241b5397d30d3a86485fa994a
github-cloudlabsuser-2108 <169078283+github-cloudlabsuser-2108@users.noreply.github.com>
2024-10-07 12:41:21 +0000

    Copilot third commit

---
+++ terraform.tf
@@ -0,0 +1,31 @@
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Define variables
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+}
+
+variable "location" {
+  description = "Location for the storage account"
+  type        = string
+  default     = "West Europe"
+}
+
+# Create the storage account
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Create a resource group (if not already existing)
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = var.location
+}
